ef9ab24a1e2270ef62384ec13976f4d1fe296f13
Tom Hinton <tomjhinton@gmail.com>
2024-02-05 10:54:57 +0000

    hoya

--- src/Experience.js
+++ src/Experience.js
@@ -70,7 +70,6 @@ const particles = useMemo(() => {
   const velocities = [];
 
   for (let i = 0; i < particlesCount; i++) {
-    positions.push(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
     velocities.push(Math.random()  , Math.random()  , Math.random()  );
   }
 
@@ -79,42 +78,39 @@ const particles = useMemo(() => {
 
 useFrame(() => {
   const positions = ref.current.geometry.attributes.position.array;
-    let averageA = [-5,-5,-5]
+    let averageA = [0,0,0]
+
+
   for (let i = 0; i < positions.length /3; i++) {
     const i3 = i * 3;
- 
-    if(particles[i3] >1){
-      particles[i3]*=.5
-    }
-    if(particles[i3+1] >1){
-      particles[i3+1]*=.5
-    }
-   
-    if(particles[i3+2] >1){
-      particles[i3+2]*=.5
-    }
-   
-   
 
-    positions[i3] += particles.velocities[i3] * 0.01;
-    positions[i3 + 1] += particles.velocities[i3 + 1] * 0.01;
-    positions[i3 + 2] += particles.velocities[i3 + 2] * 0.01;
+
+    // Stop max speed
+ 
+    if(Math.abs(particles.velocities[i3]) >20.){
+      particles.velocities[i3]*=.5
+    }
+    if(Math.abs(particles.velocities[i3+1]) >20.){
+      particles.velocities[i3+1]*=.5
+    }
+   
+    if(Math.abs(particles.velocities[i3+2]) >20.){
+      particles.velocities[i3+2]*=.5
+    }
+   
+   
+// Average Direction
+    positions[i3] += particles.velocities[i3] * 0.02;
+    positions[i3 + 1] += particles.velocities[i3 + 1] * 0.02;
+    positions[i3 + 2] += particles.velocities[i3 + 2] * 0.02;
+    
+    
+    
     averageA[0] += positions[i3]
     averageA[1] += positions[i3+1]
     averageA[2] += positions[i3+2]
 
 
-  //   for (let j = 0; j < positions.length/3; j++) {
-  //     const j3 = j * 3;
-
-  //     if(Math.abs(positions[i3] - positions[j3]) < .01 && Math.abs(positions[i3+1] - positions[j3+1]) < .01 && Math.abs(positions[i3+2] - positions[j3+2]) < .01){
-  //         particles.velocities[i3] *= -1;
-          
-
-  //     }
-
-
-  // }
   var pointA = new THREE.Vector3(positions[i3],positions[i3+1],positions[i3+2]);
   var pointB = new THREE.Vector3(averageA[0], averageA[1],averageA[2]);
  
@@ -124,25 +120,40 @@ useFrame(() => {
 // Normalize the direction vector
 directionVector.normalize();
 
-// Now the directionVector contains the direction from pointA to pointB
-console.log(directionVector);
+
 
 particles.velocities[i3] += directionVector.x *.1
 particles.velocities[i3+1] += directionVector.y *.1
 particles.velocities[i3+2] += directionVector.z *.1
 
 
-
+// console.log(particles.velocities[i3])
 
   // particles[i3] = direction.subVectors( [positions[i3],positions[i3+1],positions[i3+2]], averageA )[0] ;
   // particles[i3+1] = direction.subVectors( [positions[i3],positions[i3+1],positions[i3+2]], averageA )[1] ;
   // particles[i3+2] = direction.subVectors( [positions[i3],positions[i3+1],positions[i3+2]], averageA )[2] ;
 
-  if(Math.abs(averageA[0] - positions[i3]) < .1 && Math.abs(averageA[1] - positions[i3+1]) < .1 && Math.abs(averageA[2] - positions[i3+2]) < .1){
-    particles.velocities[i3] *= -1;
+//   if(Math.abs(averageA[0] - positions[i3]) < .1 && Math.abs(averageA[1] - positions[i3+1]) < .1 && Math.abs(averageA[2] - positions[i3+2]) < .1){
+//     particles.velocities[i3] *= -1;
     
 
+// }
+
+
+// Proximity
+    for (let j = 3; j < positions.length/3; j++) {
+      const j3 = j * 3;
+
+
+  if(Math.abs(positions[j3] - positions[i3]) < .001 && Math.abs(positions[j3+1] - positions[i3+1]) < .001 && Math.abs(positions[j3+2] - positions[i3+2]) < .001){
+    // particles.velocities[i3 + Math.floor(Math.random() * 3)] *= -1;
+    console.log(positions[j3] - positions[i3])
+
 }
+    
+}
+
+
    
 
    
@@ -164,34 +175,6 @@ particles.velocities[i3+2] += directionVector.z *.1
     } 
 
 
-    // if (positions[i3] <averageA[0] ){
-    //   particles.velocities[i3] *= -1;
-      
-    // } 
-  //   if (positions[i3+1] <averageA[1] ){
-  //     // particles.velocities[i3] *= -1;
-  //     particles.velocities[i3 + 1] *= -1;
-  //     // particles.velocities[i3 + 2] *= -1;
-  //   }
-    
-  //  if (positions[i3+2] <averageA[2] ){
-  //     // particles.velocities[i3] *= -1;
-  //     // particles.velocities[i3 + 1] *= -1;
-  //     particles.velocities[i3 + 2] *= -1;
-  //   } 
-
-
-
-   
-    
-
-    
-
-    // if (positions[i3] >  positions[i3-3] ){
-    //     particles.velocities[i3] *= -1;
-    //     particles.velocities[i3 + 1] *= -1;
-    //     particles.velocities[i3 + 2] *= -1;
-    //   } 
   }
 
      averageA[0] /= positions.length/3
